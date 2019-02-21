a0e583b236469904b69d9ce7666dc2f57ffa607d
OctoMiao <lei.ma@homelike.cc>
2019-02-21 23:50:33 +0100

    added data api as submodule

--- pipelines/insight-weather.py
+++ pipelines/insight-weather.py
@@ -6,14 +6,30 @@ from libs.insight_weather import clean_up_response as _clean_up_response
 
 __location__ = os.path.realpath( os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-data_file_path = os.path.join(__location__, 'data/insight-weather.json')
+full_data_file_path = os.path.join(__location__, '../dapi/insight-weather/all.json')
+recent_data_file_path = os.path.join(__location__,  '../dapi/insight-weather/recent.json')
 
 url_base = 'https://mars.nasa.gov/rss/api/?feed=weather&category=insight&feedtype=json&ver=1.0'
 
-data = _PersistentData(data_file_path, format='json')
+recent_data_dict = _get_remote(url_base)
 
-#get_remote(url_base)
+# 
+recent_data = _PersistentData(recent_data_file_path, format='json')
+recent_data.clear()
+recent_data = _clean_up_response(recent_data_dict, loaded_data=recent_data )
+recent_data.sync()
 
-data = _clean_up_response(_get_remote(url_base), loaded_data=data)
+full_data = _PersistentData(full_data_file_path, format='json')
+full_data = _clean_up_response(_get_remote(url_base), loaded_data=full_data)
+full_data.sync()
 
-data.sync()
+
+
+records_data_file_path = os.path.join(
+    __location__,  
+    '../dapi/insight-weather/records/{}.json'
+    .format(max([ int(key) for key in recent_data_dict.get('sol_keys') ])) 
+    )
+records_data = _PersistentData(records_data_file_path, format='json')
+records_data = _clean_up_response(recent_data_dict, loaded_data=records_data )
+records_data.sync()
